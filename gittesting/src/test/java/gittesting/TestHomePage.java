c80e2608e4d795fcbaf147f7dc9b617813ceabfb
Sanju <sanju@sunfra.com>
2015-11-06 12:01:13 +0530

    Acchutha updated

---
+++ gittesting/src/test/java/gittesting/TestHomePage.java
@@ -0,0 +1,35 @@
+package gittesting;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TestHomePage {
+	public static WebDriver driver;
+	 @Before
+		public void setUp() throws Exception{
+		 System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+	        ChromeOptions options = new ChromeOptions();
+	        options.addArguments("--test-type");        
+	        options.addArguments("start-maximized");
+	        driver = new ChromeDriver(options);
+	        driver.get("http://www.amazon.in");
+	 }
+	 @Test
+	 public void testSample()throws Exception{
+		 assertTrue("amazon page is not displayed", driver.findElement(By.xpath("//*[@id='nav-logo']")).isDisplayed());
+	 }
+	 @After
+	 public void tearDown() throws Exception {
+		 driver.quit();
+	 }
+	 
+
+}
